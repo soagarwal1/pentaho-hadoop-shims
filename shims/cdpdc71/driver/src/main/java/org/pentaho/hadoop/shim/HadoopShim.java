35acd1f7b79f97def0685ac8584913dba7b35e34
soagarwal1 <sourav.agarwal@hitachivantara.com>
2025-02-03 09:56:54 -0600

    latest fix

--- shims/cdpdc71/driver/src/main/java/org/pentaho/hadoop/shim/HadoopShim.java
+++ shims/cdpdc71/driver/src/main/java/org/pentaho/hadoop/shim/HadoopShim.java
@@ -14,10 +14,13 @@
 package org.pentaho.hadoop.shim;
 
 
+import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.core.Versioned;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.Message;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.ImplicitContextKeyed;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.hadoop.hbase.CompatibilityFactory;
 import org.apache.hadoop.hbase.HConstants;
@@ -25,15 +28,15 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.ipc.RpcServer;
 import org.apache.hadoop.hbase.mapreduce.JobUtil;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
-import org.apache.hadoop.hbase.metrics.MetricRegistry;
 import org.apache.hadoop.hbase.metrics.Snapshot;
 import org.apache.hadoop.hbase.metrics.impl.FastLongHistogram;
+import org.apache.hadoop.hbase.unsafe.HBasePlatformDependent;
 import org.apache.hadoop.hbase.zookeeper.ZKWatcher;
+import org.apache.hbase.thirdparty.com.google.common.cache.CacheLoader;
 import org.apache.hbase.thirdparty.com.google.common.collect.Lists;
 import org.apache.hbase.thirdparty.com.google.protobuf.UnsafeByteOperations;
 import org.apache.hbase.thirdparty.io.netty.channel.Channel;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.hbase.thirdparty.com.google.common.cache.CacheLoader;
 import org.pentaho.di.core.exception.KettlePluginException;
 import org.pentaho.hadoop.shim.common.HadoopShimImpl;
 
@@ -60,8 +63,8 @@ public class HadoopShim extends HadoopShimImpl {
       RpcServer.class, CompatibilityFactory.class, JobUtil.class, TableMapper.class, FastLongHistogram.class,
       Snapshot.class, ZooKeeper.class, Channel.class, Message.class, UnsafeByteOperations.class, Lists.class,
       MetricRegistry.class, ArrayUtils.class, ObjectMapper.class, Versioned.class,
-      JsonView.class, ZKWatcher.class, CacheLoader.class
+      JsonView.class, ZKWatcher.class, CacheLoader.class, HBasePlatformDependent.class, Span.class,
+      ImplicitContextKeyed.class
     };
   }
-
 }
